7741701e305ef8270c74d8b58de462d05a2ae5f7
Haneen Mahdin <haneenmahdin@gmail.com>
2021-12-26 21:31:52 +0530

    Add animation to ActionSheet and visible prop

--- src/styles/ActionSheet.js
+++ src/styles/ActionSheet.js
@@ -1,16 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const MoveIn = keyframes`
+    from {
+        bottom: -100%;
+    }
+`;
 
 const ActionSheet = styled.div`
+    display: ${p => p.visible ? "block" : "none"};
     position: fixed;
     bottom: 0;
     left: 50%;
     transform: translate(-50%, 0);
-    height: 85vh;
+    height: 75vh;
     width: 90vw;
     background: #fff;
     border-radius: 20px 20px 0 0;
     box-shadow: 0 0 0 1000px #68612350;
     z-index: 1000;
+    animation: ${MoveIn} 1s ease-in-out;
 `;
 
 export default ActionSheet;
